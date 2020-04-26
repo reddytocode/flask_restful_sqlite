d04c3905474c092702aac2ed00258d0ee9937bba
Reddy Tintaya <aaabeeelooon@gmail.com>
2020-04-26 14:47:35 -0400

    req

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
 app.secret_key = 'reddy_secret_key'
 api = Api(app)
 
